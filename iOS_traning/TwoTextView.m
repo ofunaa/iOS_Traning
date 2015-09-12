4349c31bb9e529e1ef214aea3eee2348f4b46c65
takujifunao <takuji.funao@gmail.com>
2015-09-12 11:56:43 +0900

    add NSLog

--- iOS_traning/TwoTextView.m
+++ iOS_traning/TwoTextView.m
@@ -15,6 +15,10 @@
     NSString * content = [[NSString alloc] init];
     content = self.firstTextView.text;
     self.secondTextView.text = content;
+    
+    NSLog(@"文字列のログ%@", content);
+    NSLog(@"整数のログ%d", 100);
+    NSLog(@"少数のログ%f", 1.993939);
 }
 
 @end
